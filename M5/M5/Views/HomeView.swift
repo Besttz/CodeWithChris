90b0a8040319b65642b97e53f2298e619056612d
Tommy <tommyzhangchina@gmail.com>
2022-10-20 22:18:56 +1100

    M5L4: Build the HomeView

--- M5/M5/Views/HomeView.swift
+++ M5/M5/Views/HomeView.swift
@@ -12,18 +12,32 @@ struct HomeView: View {
     @EnvironmentObject var model:ContentModel
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("What do we want to do today?").padding(.horizontal)
+                ScrollView {
+                    LazyVStack {
+        //                    .padding(.leading,20)
+                        ForEach(model.modules) { module in
+                            VStack(spacing: 20) {
+                                HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, lessons: module.content.lessons.count, time: module.content.time)
+                                HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, lessons: module.content.lessons.count, time: module.content.time)
+                            }
+                        }.padding()
+                        
+                        
+                    }
+                }
+            }.navigationTitle("Lessons")
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
-            .environmentObject(ContentModel())
+        NavigationView {
+            HomeView()
+                .environmentObject(ContentModel())
+        }
     }
 }
---
+++ M5/M5/Views/HomeViewRow.swift
@@ -0,0 +1,61 @@
+//
+//  HomeViewRow.swift
+//  M5
+//
+//  Created by ZhangTommy on 20/10/2022.
+//
+
+import SwiftUI
+
+struct HomeViewRow: View {
+    var image : String
+    var title : String
+    var description : String
+    var lessons : Int
+    var time: String
+    
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .aspectRatio(CGSize(width: 335, height: 175),contentMode: .fit)
+            HStack(alignment: .center){
+                Image(image)
+                    .resizable()
+                    .foregroundColor(.accentColor)
+                    .clipShape(Circle())
+                    .frame(width: 130,height: 130)
+                
+                VStack(alignment: .leading) {
+                    Text(title)
+                        .font(.title2)
+                        .padding(.bottom, 5)
+                    Text(description)
+                        .font(.caption)
+                        .padding(.bottom,20 )
+                    Spacer()
+                    HStack{
+                        Image(systemName: "text.book.closed")
+                            .resizable()
+                            .frame(width: 12,height: 12)
+                        Text("\(lessons) Lessons")
+                            .font(.caption)
+                        Spacer()
+                        Image(systemName: "clock")
+                            .resizable()
+                            .frame(width: 12,height: 12)
+                        Text(time)
+                            .font(.caption)
+                    }
+                    Spacer()
+
+                }.padding()
+            }.padding()
+            
+        }
+    }
+}
+
+
